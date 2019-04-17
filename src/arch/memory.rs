52ac6082843e2e52e66db7c39f75f89db1738a1a
yufrice <c1155208@cstu.nit.ac.jp>
2019-04-17 17:09:41 +0900

    add PPUレジスタとVRAM直結

--- src/arch/memory.rs
+++ src/arch/memory.rs
@@ -1,3 +1,4 @@
+use std::ops::Not;
 use std::cell::{Cell, RefCell};
 
 use crate::arch::RcRefCell;
@@ -60,9 +61,8 @@ impl CPUMemory {
                         32
                     };
                     let addr = ppu_reg.PPUADDR.get();
-                    let PPUMemory(ref vram) = ppu_reg.PPUDATA;
                     ppu_reg.PPUADDR.set(addr + counter);
-                    vram.borrow()[usize::from(addr)]
+                    ppu_reg.PPUDATA.read(usize::from(addr))
                 }
                 _ => unreachable!(),
             }
@@ -104,22 +104,40 @@ impl CPUMemory {
                 0x2000 => ppu_reg.PPUCTRL.set(value),
                 0x2001 => ppu_reg.PPUMASK.set(value),
                 0x2002 => unreachable!(),
-                0x2003 => ppu_reg.OAMADDR.set(value),
+                0x2003 => {
+                ppu_reg.oamaddr_bit_flag.set(!ppu_reg.oamaddr_bit_flag.get());
+                ppu_reg.OAMADDR.set(value);
+                }
                 0x2004 => {
+                    let pre_adr = ppu_reg.oamaddr_bit_flag.get();
+                    // ToDo u16空間用意してhighはシフト
+                    let value = match ppu_reg.oamaddr_bit_flag.get() {
+                        BitFlag::Low => value,
+                        BitFlag::High => value,
+                    };
                     ppu_reg.OAMDATA.set(value);
                     ppu_reg.OAMADDR.set(ppu_reg.OAMADDR.get() + 1);
                 }
                 0x2005 => ppu_reg.PPUSCROLL.set(value),
-                0x2006 => ppu_reg.PPUADDR.set(value),
+                0x2006 => {
+                    let pre_adr = ppu_reg.PPUADDR.get();
+                    let value = match ppu_reg.ppuaddr_bit_flag.get() {
+                        BitFlag::Low => pre_adr + u16::from(value),
+                        BitFlag::High => u16::from(value).rotate_left(8),
+                    };
+                    ppu_reg.PPUADDR.set(value);
+                    ppu_reg.ppuaddr_bit_flag.set(!ppu_reg.ppuaddr_bit_flag.get());
+                },
                 0x2007 => {
                     let addr = ppu_reg.PPUADDR.get();
-                    let PPUMemory(ref vram) = ppu_reg.PPUDATA;
-                    vram.borrow_mut()[usize::from(addr)] = value;
+                    ppu_reg.PPUDATA.write(usize::from(addr), value);
+
                     let counter = if 0 != (ppu_reg.PPUCTRL.get() & 0x02) {
                         1
                     } else {
                         32
                     };
+
                     let addr = ppu_reg.PPUADDR.get();
                     ppu_reg.PPUADDR.set(addr + counter);
                 }
@@ -138,7 +156,7 @@ impl CPUMemory {
 }
 
 // VRAM E0117
-pub(crate) struct PPUMemory(pub(crate) RefCell<[u8; 0x10000]>);
+pub(crate) struct PPUMemory(pub(crate) RefCell<[u8; 0x4000]>);
 
 impl std::fmt::Debug for PPUMemory {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -148,10 +166,60 @@ impl std::fmt::Debug for PPUMemory {
 
 impl Default for PPUMemory {
     fn default() -> Self {
-        Self(RefCell::new([0x00; 0x10000]))
+        Self(RefCell::new([0x00; 0x4000]))
     }
 }
 
+impl PPUMemory {
+    pub(crate) fn read(&self, addr: usize) -> u8 {
+        let PPUMemory(vram) = self;
+
+        let addr = match addr {
+            0x0000 ... 0x27C0 => addr,
+            0x2800 ... 0x2FBF => addr - 0x0800,
+            0x2FC0 ... 0x2FFF => addr - 0x08C0,
+            0x3000 ... 0x3EFF => unreachable!(),
+            0x3F00 ... 0x3F1F => addr,
+            _ => unreachable!(),
+        };
+
+        vram.borrow()[addr]
+    }
+
+    pub(crate) fn write(&self, addr: usize, value: u8) {
+        let PPUMemory(vram) = self;
+
+        let addr = match addr {
+            0x0000 ... 0x27C0 => addr,
+            0x2800 ... 0x2FBF => addr - 0x0800,
+            0x2FC0 ... 0x2FFF => addr - 0x08C0,
+            0x3000 ... 0x3EFF => unreachable!(),
+            0x3F00 ... 0x3F1F => addr,
+            0x3F20 ... 0x3FFF => addr - 0x20,
+            0x4000 ... 0xFFFF => addr - 0xC00,
+            _ => unreachable!("PPU Write Address: {:X}", addr),
+        };
+
+        vram.borrow_mut()[addr] = value;
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub(crate) enum BitFlag {
+    Low,
+    High,
+}
+
+impl Not for BitFlag {
+    type Output = BitFlag;
+
+    fn not(self) -> BitFlag {
+        match self {
+            BitFlag::Low => BitFlag::High,
+            BitFlag::High => BitFlag::Low,
+        }
+    }
+}
 
 /// PPU Register
 #[derive(Debug)]
@@ -186,6 +254,7 @@ pub(crate) struct PPURegister {
     /// $2003 Write  
     /// スプライトの書き込み先
     pub OAMADDR: Cell<u8>,
+    pub oamaddr_bit_flag: Cell<BitFlag>,
     /// スプライトデータ
     /// $2004 Read Write  
     /// $2003に送るスプライトを指定  
@@ -195,7 +264,8 @@ pub(crate) struct PPURegister {
     pub PPUSCROLL: Cell<u8>,
     /// Write VRAM Address
     /// High/Low Address
-    pub PPUADDR: Cell<u8>,
+    pub PPUADDR: Cell<u16>,
+    pub ppuaddr_bit_flag: Cell<BitFlag>,
     /// Read Write VRAM
     pub PPUDATA: PPUMemory,
 }
@@ -207,9 +277,11 @@ impl Default for PPURegister {
             PPUMASK: Cell::new(0x00),
             PPUSTATUS: Cell::new(0x00),
             OAMADDR: Cell::new(0x00),
+            oamaddr_bit_flag: Cell::new(BitFlag::High),
             OAMDATA: Cell::new(0x00),
             PPUSCROLL: Cell::new(0x00),
             PPUADDR: Cell::new(0x00),
+            ppuaddr_bit_flag: Cell::new(BitFlag::High),
             PPUDATA: PPUMemory::default(),
         }
     }

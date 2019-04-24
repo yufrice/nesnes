f116a8bc08000bc7c41af756cedddc06594e9db6
yufrice <contact@yufarch.com>
2019-04-24 21:39:39 +0900

    ステージング漏れ

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,5 @@ extern crate nesnes;
 fn main() {
     env::set_var("RUST_LOG", "info");
     env_logger::init();
-
-    match nesnes::parser::parser("test0.nes") {
-        Ok(arch) => nesnes::ui::run(arch),
-        Err(err) => println!("{:?}", err),
-    }
+    nesnes::ui::run();
 }

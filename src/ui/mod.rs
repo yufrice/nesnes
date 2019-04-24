0b1d14392960759d4e2b455feb793336f59ceef4
yufrice <contact@yufarch.com>
2019-04-24 21:57:21 +0900

    clean

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -5,9 +5,9 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
-
 use sdl2::render::TextureQuery;
 use std::time::Duration;
+
 use crate::parser;
 
 pub fn run() {

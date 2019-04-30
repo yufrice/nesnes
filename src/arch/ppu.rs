374a95b665ea36d98b509b1d31fc22442251345d
yufrice <contact@yufarch.com>
2019-04-30 19:44:27 +0900

    wip

--- src/arch/ppu.rs
+++ src/arch/ppu.rs
@@ -1,21 +1,24 @@
 use log::info;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
-use sdl2::render::Canvas;
+use sdl2::render::{Canvas, Texture};
 use sdl2::video::Window;
 use std::cell::RefCell;
 
 use crate::arch::memory::{PPUMemory, PPURegister};
 use crate::arch::RcRefCell;
+use crate::{
+    DISPLAY_HEIGHT, DISPLAY_SIZE, DISPLAY_SPRITE_WIDTH, DISPLAY_WIDTH, PATTERN_LENGTH, SPRITE,
+    SPRITE_SIDE,
+};
 
-/// Sprite(0x40) x 0x200
-pub type Pattern = [[u8; 0x40]; 0x200];
+pub type Pattern = [[u8; SPRITE]; PATTERN_LENGTH];
 
 pub(crate) struct PPU {
     /// CHR
     pub(crate) pattern0: Pattern,
     pub(crate) state: RefCell<PPUState>,
-    pub(crate) display: RefCell<[u8; 61440]>,
+    pub(crate) display: RefCell<Texture>,
     pub(crate) ioc: RcRefCell<PPURegister>,
     pub(crate) canvas: RcRefCell<Canvas<Window>>,
 }
@@ -26,7 +29,8 @@ impl PPU {
         ioc: RcRefCell<PPURegister>,
         canvas: RcRefCell<Canvas<Window>>,
     ) -> PPU {
-        fn parse_sprite(buffer: &mut [[u8; 0x40]; 0x200], chr: Vec<u8>) {
+        fn parse_sprite(buffer: &mut [[u8; SPRITE]; PATTERN_LENGTH], chr: Vec<u8>) {
+            // 16bit -> (8bit, 8bit) -> sprite
             for (sprite_idx, sprite) in chr.chunks(16).enumerate() {
                 let (pixel0, pixel1) = sprite.split_at(8);
                 for (idx, (pix0, pix1)) in pixel0.iter().zip(pixel1).enumerate() {
@@ -39,12 +43,22 @@ impl PPU {
             }
         }
         let state = PPUState::default();
-        let buffer = &mut [[0u8; 0x40]; 0x200];
+        let buffer = &mut [[0u8; SPRITE]; PATTERN_LENGTH];
+
+        let texture_creator = canvas.borrow().texture_creator();
+        let mut texture = texture_creator
+            .create_texture_streaming(
+                PixelFormatEnum::RGB24,
+                DISPLAY_WIDTH as u32,
+                DISPLAY_HEIGHT as u32,
+            )
+            .unwrap();
+
         parse_sprite(buffer, chr);
         PPU {
             pattern0: *buffer,
             state: RefCell::new(state),
-            display: RefCell::new([0; 61440]),
+            display: RefCell::new(texture),
             /// I/O CPU Register
             ioc,
             canvas,
@@ -61,6 +75,8 @@ impl PPU {
             state.borrow_mut().line += 1;
             match line {
                 0...239 if line % 8 == 0 => self.sprite_generate(),
+                // 描画
+                240 => self.flush_sprite(),
                 262 => state.borrow_mut().line = 0,
                 _ => (),
             };
@@ -81,6 +97,7 @@ impl PPU {
     }
 
     pub(crate) fn sprite_generate(&self) {
+        // RGB 0x40色
         static color_palettes: [[u8; 0x03]; 0x40] = [
             [84, 84, 84],
             [0, 30, 116],
@@ -148,42 +165,54 @@ impl PPU {
             [0, 0, 0],
         ];
 
-        const SPRITE_WIDTH: u32 = 32;
-        const SPRITE_HEIGHT: u32 = 30;
-        const WIDTH: u32 = 8 * SPRITE_WIDTH;
-        const HEIGHT: u32 = 8 * SPRITE_HEIGHT;
-
         let PPUMemory(ref vram) = self.ioc.borrow().ppudata;
+
         let line = self.state.borrow().line as usize / 8usize;
-        let range = line * 8..(line + 1) * 8;
+        self.display
+            .borrow_mut()
+            .with_lock(None, |buffer: &mut [u8], pitch: usize| {
+                for idx in 0..DISPLAY_SPRITE_WIDTH {
+                    let sprite_idx = line * DISPLAY_SPRITE_WIDTH + idx;
+                    for (pixel_idx, pixel) in self.pattern0[vram.borrow()[line] as usize]
+                        .iter()
+                        .enumerate()
+                    {
+                        let sprite_x_idx = sprite_idx % DISPLAY_SPRITE_WIDTH;
+                        let sprite_y_idx = line;
+                        let pixel_x_idx = pixel_idx % SPRITE_SIDE;
+                        let pixel_y_idx = pixel_idx / SPRITE_SIDE;
 
-        for line in range {
-            // マジックナンバー化してるとこなおす
-            for idx in 0..32 {
-                let sprite_idx = line * 32 + idx;
-                // カラーパレット読んでRGB行列に落とす
-                // plt = line->ram->plt
-                for (pixel_idx, pixel) in self.pattern0[vram.borrow()[line] as usize]
-                    .iter()
-                    .enumerate()
-                {
-                    let sprite_x_idx = sprite_idx % 32;
-                    let sprite_y_idx = line;
-                    let pixel_x_idx = pixel_idx % 8;
-                    let pixel_y_idx = pixel_idx / 8;
+                        let offset = sprite_x_idx * SPRITE_SIDE * 3
+                            + sprite_y_idx * 3 * DISPLAY_WIDTH
+                            + pixel_x_idx * 3
+                            + pixel_y_idx * 3 * SPRITE_SIDE * DISPLAY_SPRITE_WIDTH;
 
-                    let offset = sprite_x_idx * 24
-                        + sprite_y_idx * 3 * 8 * 32 * 8
-                        + pixel_x_idx * 3
-                        + pixel_y_idx * 3 * 8 * 32;
-                    let color = color_palettes[*pixel as usize];
+                        // ラインとスプライト位置から属性テーブル参照するようにする
+                        let color = color_palettes[*pixel as usize];
 
-                    self.display.borrow_mut()[offset] = color[0];
-                    self.display.borrow_mut()[offset+1] = color[1];
-                    self.display.borrow_mut()[offset|2] = color[2];
+                        buffer[offset] = color[0];
+                        buffer[offset + 1] = color[1];
+                        buffer[offset + 2] = color[2];
+                    }
                 }
-            }
-        }
+            })
+            .unwrap();
+    }
+
+    fn flush_sprite(&self) {
+        self.canvas
+            .borrow_mut()
+            .copy(
+                &self.display.borrow(),
+                None,
+                Some(Rect::new(
+                    0,
+                    0,
+                    2 * DISPLAY_WIDTH as u32,
+                    2 * DISPLAY_HEIGHT as u32,
+                )),
+            )
+            .unwrap();
     }
 }
 
@@ -197,10 +226,4 @@ impl Default for PPUState {
     fn default() -> Self {
         Self { cycle: 0, line: 0 }
     }
-}
-
-pub(crate) fn position_map(idx: i32) -> (usize, usize) {
-    let x_idx = idx % 32 * 8 * 3;
-    let y_idx = idx / 32 * 8 * 3 * 32;
-    (x_idx as usize, y_idx as usize)
 }

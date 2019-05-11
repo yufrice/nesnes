dba80e13f604b1c4d79a1e68b846ff449fd9a241
yufrice <contact@yufarch.com>
2019-05-11 21:29:16 +0900

    wip

--- src/arch/ppu.rs
+++ src/arch/ppu.rs
@@ -14,6 +14,13 @@ use crate::{
 
 pub type Pattern = [[u8; SPRITE]; PATTERN_LENGTH];
 
+pub(crate) enum DisplayID {
+    DISPLAY1,
+    DISPLAY2,
+    DISPLAY3,
+    DISPLAY4,
+}
+
 pub(crate) struct PPU {
     /// CHR
     pub(crate) pattern0: Pattern,
@@ -103,6 +110,87 @@ impl PPU {
         }
     }
 
+    pub(crate) fn get_attribute(&self, line: usize, sprite_idx: usize, display: DisplayID) {
+        // RGB 0x40色
+        static color_palettes: [[u8; 0x03]; 0x40] = [
+            [84, 84, 84],
+            [0, 30, 116],
+            [8, 16, 144],
+            [48, 0, 136],
+            [68, 0, 100],
+            [92, 0, 48],
+            [84, 4, 0],
+            [60, 24, 0],
+            [32, 42, 0],
+            [8, 58, 0],
+            [0, 64, 0],
+            [0, 60, 0],
+            [0, 50, 60],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [152, 150, 152],
+            [8, 76, 196],
+            [48, 50, 236],
+            [92, 30, 228],
+            [136, 20, 176],
+            [160, 20, 100],
+            [152, 34, 32],
+            [120, 60, 0],
+            [84, 90, 0],
+            [40, 114, 0],
+            [8, 124, 0],
+            [0, 118, 40],
+            [0, 102, 120],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [236, 238, 236],
+            [76, 154, 236],
+            [120, 124, 236],
+            [176, 98, 236],
+            [228, 84, 236],
+            [236, 88, 180],
+            [236, 106, 100],
+            [212, 136, 32],
+            [160, 170, 0],
+            [116, 196, 0],
+            [76, 208, 32],
+            [56, 204, 108],
+            [56, 180, 204],
+            [60, 60, 60],
+            [0, 0, 0],
+            [0, 0, 0],
+            [236, 238, 236],
+            [168, 204, 236],
+            [188, 188, 236],
+            [212, 178, 236],
+            [236, 174, 236],
+            [236, 174, 212],
+            [236, 180, 176],
+            [228, 196, 144],
+            [204, 210, 120],
+            [180, 222, 120],
+            [168, 226, 144],
+            [52, 226, 180],
+            [160, 214, 228],
+            [160, 162, 160],
+            [0, 0, 0],
+            [0, 0, 0],
+        ];
+
+        const BASE_ADDR_DISPLAY1: usize = 0x23C0;
+        const BASE_ADDR_DISPLAY2: usize = 0x27C0;
+        let addr = match display {
+            DisplayID::DISPLAY1 => BASE_ADDR_DISPLAY1,
+            DisplayID::DISPLAY2 => BASE_ADDR_DISPLAY2,
+            DisplayID::DISPLAY3 => BASE_ADDR_DISPLAY1,
+            DisplayID::DISPLAY4 => BASE_ADDR_DISPLAY2,
+        } + sprite_idx/4 + line/4 * 8;
+        let attr = self.ioc.borrow().ppudata.read(addr);
+
+    }
+
     pub fn sprite_flush(&self) -> Pattern {
         self.pattern0
     }
@@ -184,6 +272,7 @@ impl PPU {
             .with_lock(None, |buffer: &mut [u8], pitch: usize| {
                 for idx in 0..DISPLAY_SPRITE_WIDTH {
                     let sprite_idx = line * DISPLAY_SPRITE_WIDTH + idx;
+                    self.get_attribute(line, sprite_idx % DISPLAY_SPRITE_WIDTH, DisplayID::DISPLAY1);
                     for (pixel_idx, pixel) in self.pattern0[vram.read(sprite_idx + 0x2000) as usize]
                         .iter()
                         .enumerate()
